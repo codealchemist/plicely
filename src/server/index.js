14936b203ba4972ebd0f0ed2161d288f4265887f
Alberto Miranda <codealchemist@gmail.com>
2018-12-31 13:17:23 -0300

    Removed https-enforcer, doing it on CloudFlare.

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const httpsEnforcer = require('https-enforcer')
 
 const app = express()
 const port = process.env.PORT || 9090
 
 function setRoutes () {
-  app.use(httpsEnforcer)
   app.use(express.static('dist'))
 
   app.use((req, res) => {
