6f143b8b3495e8c27282f2b71d3daa4554f28671
Alberto Miranda <codealchemist@gmail.com>
2019-02-06 23:53:09 -0300

    Added google analytics; new build.

--- dist/sw.js
+++ dist/sw.js
@@ -19,11 +19,11 @@ var __wpo = {
     "f720b6244b0fb73f345299c0feed4d08556c8cc5": "./76e5a546b96c58b6854a8f217848808e.png",
     "70d149f92b72b1cff4f02898b6066228171d2c42": "./6503c1267b000ace2e068edeee2de368.png",
     "155470a577717803f975f96c2216b51e82335579": "./bundle.js",
-    "9e27ae8a13c9e847cf9743e95c7a19b014f3410b": "./"
+    "aaddf82472ce94c277fc1d17c97c3ffaa6984686": "./"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2019-1-17 21:22:00",
+  "version": "2019-2-6 23:51:46",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": true
